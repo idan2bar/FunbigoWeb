b20ea71d7123aed6dacd242b0afd21be85827397
Idan Bar <idan2bar@gmail.com>
2025-10-05 16:45:29 +0300

    Added redirects

--- FunbigoWebsite/astro.config.mjs
+++ FunbigoWebsite/astro.config.mjs
@@ -2,4 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://www.funbigo.com",
+    redirects: {
+        // HTTP 302 - Temporary Redirect (for allowing future changes)
+        '/qr': { destination: 'https://linktr.ee/funbigo', status: 302 },
+        '/community': { destination: 'https://linktr.ee/funbigo', status: 302 },
+        '/discord': { destination: 'https://discord.gg/T59A7NjwKz', status: 302 },
+        '/whatsapp': { destination: 'https://chat.whatsapp.com/GjSQyTo90jCLDLC2YqqUpo', status: 302 },
+        '/googlePlay': { destination: 'https://play.google.com/store/apps/details?id=com.Funbigo', status: 302 },
+    }
+});
