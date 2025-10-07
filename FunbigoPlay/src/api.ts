a4cd2a0319e210a8fa2c67a4b198779c93f2ec98
Idan Bar <idan2bar@gmail.com>
2025-10-07 10:28:50 +0300

    Organized code and added a 404 page

--- FunbigoPlay/src/api.ts
+++ FunbigoPlay/src/api.ts
@@ -1,5 +1,28 @@
 ﻿
+export type ProjectData = {
+    Id: string;
+    Name: string;
+    IconId: string;
+    MetaData: {
+        shortDescription: string;
+    };
+};
 
-async function getProjectData(projectId: string): Promise<any> {
-    // return apiClient.get(`/projects/${projectId}`)
+const API_BASE_URL = 'https://api.funbigo.com';
+const ASSET_ENDPOINT = `${API_BASE_URL}/asset`;
+const ASSET_DOWNLOAD_ENDPOINT = `${API_BASE_URL}/asset/download`;
+
+export async function getProjectData(projectId: string): Promise<ProjectData | null> {
+    const response: Response = await fetch( `${ASSET_ENDPOINT}/${projectId}`);
+
+    if (response.ok) {
+        return response.json();
+    }
+    
+    console.error(`Failed to fetch project data: ${response.statusText}`);
+    return null;
+}
+
+export function getFunbigoFileUrl(fileId: string): string {
+    return `${ASSET_DOWNLOAD_ENDPOINT}/${fileId}`;
 }
---
+++ FunbigoPlay/src/consts.ts
@@ -0,0 +1,5 @@
+﻿
+export const FUNBIGO_GOOGLE_PLAY_URL: string = "https://play.google.com/store/apps/details?id=com.Funbigo";
+export const FUNBIGO_APP_PACKAGE_NAME: string = "com.Funbigo";
+export const FUNBIGO_PLAY_HOST: string = "play.funbigo.com";
+export const FUNBIGO_SITE_NAME: string = "Funbigo";
